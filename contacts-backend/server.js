7fa8ae5523e34a749066a989d26886176a6c3a79
Ragul-05 <ragulgopinath2005@gmail.com>
2025-04-17 13:40:27 +0530

    Finished project

--- contacts-backend/server.js
+++ contacts-backend/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const errorHandler = require('./middleware/errorHandler');
-const connectDb = require('./config/dbConnection');
 const dotenv = require('dotenv').config();
+const connectDb = require('./config/dbConnection');
+const errorHandler = require("./middleware/errorHandler");
+const timeout = require('connect-timeout');
 
 
 connectDb();
@@ -10,9 +11,25 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use("/api/contacts", require("./routes/contactRoutes")); 
-app.use(errorHandler);
+app.use(timeout('30s')); // Increase timeout limit to 30 seconds
+
+// Middleware to handle timeout error
+app.use((req, res, next) => {
+  if (!req.timedout) next();
+});
+
+app.use("/api/contacts",  require('./routes/contactRoutes'));
+app.use("/api/users", require("./routes/userRoutes"));
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  if (req.timedout) {
+    res.status(503).send('Service unavailable. Request timed out.');
+  } else {
+    errorHandler(err, req, res, next);
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
